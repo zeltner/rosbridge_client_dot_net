3817520a8bd0f258ce0a13e5b22cf99427c219ae
Stefan Zeltner <zeltner@usc.edu>
2014-06-25 12:58:20 -0700

    Interface for Rosbridge message serializer

---
+++ rosbridge_client_common/IMessageSerializer.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosbridge.Client
+{
+    public interface IMessageSerializer
+    {
+        /// <summary>
+        /// The rosbridge protocol version of this serializer
+        /// </summary>
+        string ProtocolVersion { get; }
+
+        /// <summary>
+        /// Serialize a message to a buffer
+        /// </summary>
+        /// <param name="message">The message to serialize</param>
+        /// <returns>The serialized message</returns>
+        byte[] Serialize(dynamic message);
+
+        /// <summary>
+        /// Deserialize a buffer and returns a messages.
+        /// </summary>
+        /// <param name="buffer">The buffer to deserialize</param>
+        /// <returnsThe message</returns>
+        JObject Deserialize(byte[] buffer);
+    }
+}
