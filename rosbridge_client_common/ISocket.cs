40f476758a556ef79baa166702fde5750fafffe4
Stefan Zeltner <zeltner@usc.edu>
2014-06-25 10:58:50 -0700

    Add a Rosbridge socket interface

---
+++ rosbridge_client_common/ISocket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Rosbridge.Client
+{
+    public interface ISocket : IDisposable
+    {
+        /// <summary>
+        /// Rosbridge server's URI
+        /// </summary>
+        Uri URI { get; }
+
+        /// <summary>
+        /// True if the connection to the server is established
+        /// </summary>
+        bool Connected { get; }
+
+        /// <summary>
+        /// Opens a connection to the rosbridge server
+        /// </summary>
+        void Connect();
+
+        /// <summary>
+        /// Closes a connection to the rosbridge server
+        /// </summary>
+        void Disconnect();
+
+        /// <summary>
+        /// Sends a message to the rosbridge server
+        /// </summary>
+        /// <param name="message">Mesage to Send</param>
+        /// <returns>A task which handles the send</returns>
+        Task SendAsync(byte[] buffer);
+
+        /// <summary>
+        /// Receives a message from the rosbridge server
+        /// </summary>
+        /// <returns>Output buffer</returns>
+        Task<byte[]> ReceiveAsync();
+    }
+}
