1f1191d52652698f4dcc62f611ca150e04cc2974
Stefan Zeltner <zeltner@usc.edu>
2014-06-25 12:59:32 -0700

    - Initial version of a rosbridge (protocol 2.0) serializer for Windows desktop apps - Empty version of a rosbridge (protocol 2.0) serializer for Windows store apps

---
+++ rosbridge_client_socket_for_apps/MessageSerializerV2_0.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosbridge.Client
+{
+    public class MessageSerializerV2_0 : IMessageSerializer
+    {
+        /// <summary>
+        /// The rosbridge protocol version of this serializer
+        /// </summary>
+        public string ProtocolVersion { get; private set; }
+
+        /// <summary>
+        /// Serialize a message to a buffer
+        /// </summary>
+        /// <param name="message">The message to serialize</param>
+        /// <returns></returns>
+        public byte[] Serialize(dynamic message)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deserialize a buffer and returns a messages.
+        /// </summary>
+        /// <param name="buffer">The buffer to deserialize</param>
+        /// <returnsThe message</returns>
+        public JObject Deserialize(byte[] buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Constructs a message serializer for the rosbridge v2.0
+        /// </summary>
+        public MessageSerializerV2_0()
+        {
+            ProtocolVersion = "rosbridge v2.0";
+        }
+    }
+}
---
+++ rosbridge_client_socket_for_desktop/MessageSerializerV2_0.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosbridge.Client
+{
+    public class MessageSerializerV2_0 : IMessageSerializer
+    {
+        /// <summary>
+        /// The rosbridge protocol version of this serializer
+        /// </summary>
+        public string ProtocolVersion { get; private set; }
+
+        /// <summary>
+        /// Serialize a message to a buffer
+        /// </summary>
+        /// <param name="message">The message to serialize</param>
+        /// <returns>A buffer containing the serialized message</returns>
+        /// <exception cref=" System.ArgumentNullException">message is null</exception>
+        public byte[] Serialize(dynamic message)
+        {
+            if (null == message)
+            {
+                throw new ArgumentNullException("message");
+            }
+
+            string json = JsonConvert.SerializeObject(message);
+            return Encoding.ASCII.GetBytes(json);
+        }
+
+        /// <summary>
+        /// Deserialize a buffer and returns a messages.
+        /// </summary>
+        /// <param name="buffer">The buffer to deserialize</param>
+        /// <returnsThe message</returns>
+        public JObject Deserialize(byte[] buffer)
+        {
+            if (null == buffer)
+            {
+                throw new ArgumentNullException("buffer");
+            }
+
+            string json = Encoding.ASCII.GetString(buffer, 0, buffer.Length);
+            return JObject.Parse(json);
+        }
+
+        /// <summary>
+        /// Constructs a message serializer for the rosbridge v2.0
+        /// </summary>
+        public MessageSerializerV2_0()
+        {
+            ProtocolVersion = "rosbridge v2.0";
+        }
+    }
+}
