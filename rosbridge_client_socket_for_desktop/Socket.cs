9ad1a109ec21a8088ee99fcd16e7ad069ce66f3b
Stefan Zeltner <zeltner@usc.edu>
2014-06-25 12:55:40 -0700

    Update doc

--- rosbridge_client_socket_for_desktop/Socket.cs
+++ rosbridge_client_socket_for_desktop/Socket.cs
@@ -56,7 +56,7 @@ namespace Rosbridge.Client
         /// <summary>
         /// Opens a connection to the rosbridge server
         /// </summary>
-        /// <returns>A task which completes once the connect is done</returns>
+        /// <returns>True if the connect was successful</returns>
         /// <exception cref="System.ObjectDisposedException">This socket is already _disposed</exception>
         public Task<bool> ConnectAsync()
         {
@@ -78,6 +78,7 @@ namespace Rosbridge.Client
         /// <summary>
         /// Closes a connection to the rosbridge server
         /// </summary>
+        /// <returns>True if the disconnect was successful</returns>
         /// <exception cref="System.ObjectDisposedException">This socket is already _disposed</exception>
         public Task<bool> DisconnectAsync()
         {
